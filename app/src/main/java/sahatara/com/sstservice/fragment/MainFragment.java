e73fef0e5a95ec6bfb02352ca61d784881366380
masterAUM <surashet08@hotmail.com>
2017-12-14 13:44:45 +0700

    Replace Fragment Success

--- app/src/main/java/sahatara/com/sstservice/fragment/MainFragment.java
+++ app/src/main/java/sahatara/com/sstservice/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sahatara.com.sstservice.R;
 
@@ -15,11 +16,34 @@ import sahatara.com.sstservice.R;
 
 public class MainFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+ //       Register Controller
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+ //      Replace Fragement
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }   //  onClick
+        });
+
+    }   //  Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         return view;
