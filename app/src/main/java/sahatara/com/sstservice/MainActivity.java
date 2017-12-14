aeebf31c7aa348119f4196ebe39417c6a5a7babc
masterAUM <surashet08@hotmail.com>
2017-12-14 11:11:13 +0700

    finish fragment

--- app/src/main/java/sahatara/com/sstservice/MainActivity.java
+++ app/src/main/java/sahatara/com/sstservice/MainActivity.java
@@ -1,13 +1,26 @@
 package sahatara.com.sstservice;
 
+import android.bluetooth.le.AdvertiseData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import sahatara.com.sstservice.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  // เมนหลัก ทำก่อน เสมอ
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+ //       Add Fragment To Activity
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+    }   // Main Method
+
+
+
+}   // Main Class
---
+++ app/src/main/java/sahatara/com/sstservice/fragment/MainFragment.java
@@ -0,0 +1,27 @@
+package sahatara.com.sstservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sahatara.com.sstservice.R;
+
+/**
+ * Created by Aum on 14/12/2560.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}   //Main Class
